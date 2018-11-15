9fe11c0db322569bc40c46f5eac20eddeaa26860
Adam <44759861+adanie15@users.noreply.github.com>
2018-11-15 15:20:15 -0500

    Updated
    Updated the Todo List with more functionality.

--- JSToDoList.js
+++ JSToDoList.js
@@ -1,20 +1,40 @@
 window.setTimeout(function() {
-    var todos = [];
+    var todos = ["Get rich"];
 
     var input = prompt("What would you like to do?")
 
     while(input !== "quit"){
         // handle input
-        if(input === "list"){
-            console.log(todos);
-        } else if(input === "new"){
-            // ask for new todo
-            var newTodo = prompt("Enter new todo");
-            // add to todos array
-            todos.push(newTodo);
-        } 
+        if(input === "list") {
+            printList();
+        } else if(input === "new") {
+            addTodo();
+        } else if(input === "delete") {
+            deleteTodo();
+        }
         // ask again for new input
         input = prompt("What would you like to do?")
     }
     console.log("Okay, you quit the app.")
+
+
+    function printList(){
+        console.log("*********");
+        todos.forEach(function(todo, i){
+            console.log(i + ": " + todo);
+        });
+        console.log("*********");
+    }
+
+    function addTodo(){
+        var newTodo = prompt("Enter new todo");
+        todos.push(newTodo);
+        console.log(newTodo + " added new Todo Item");
+    }
+
+    function deleteTodo(){
+        var index = prompt("Enter the index of todo to delete")
+        todos.splice(index, 1);
+        console.log("Deleted item from Todo List")
+    }
 }, 500);
